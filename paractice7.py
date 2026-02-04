f2e626c7f67bc795379eb2eabe9f0ee3ee9d89ff
yuliiayakymiv <72573299+yuliiayakymiv@users.noreply.github.com>
2026-02-04 10:15:28 +0200

    Add files via upload

---
+++ paractice7.py
@@ -0,0 +1,15 @@
+def get_all_tasks() -> list[Task]:
+    """
+    Return a copy of all tasks.
+
+    Returns:
+        list[Task]: A copy of the task list (not the original)
+
+    Example:
+        >>> tasks = get_all_tasks()
+        >>> len(tasks)
+        3
+    """
+    # TODO: Student 7 - Implement this function
+    # Hint: Return a copy using list() or _tasks[:]
+    raise NotImplementedError("Student 7: Implement get_all_tasks()")
